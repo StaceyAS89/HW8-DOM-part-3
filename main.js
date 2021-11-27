9d96347f565c037af8e092aa8746c214f77a759a
StaceyAS89 <anastasiiaforapple@gmail.com>
2021-11-27 19:12:31 +0200

    HW8 version1.1

---
+++ main.js
@@ -0,0 +1,25 @@
+
+let togBtn = document.getElementById('togBtn');
+let fullFormat = document.getElementById("demo-full-format");
+let shortFormat = document.getElementById("demo-short-format");
+
+
+
+function timerFullFormat() {
+    let timeShow = new Date();
+    fullFormat.innerHTML = timeShow.toLocaleTimeString();
+}
+
+function timerShortFormat() {
+    let timeShow = new Date();
+    shortFormat.innerHTML = timeShow.toLocaleTimeString([], {timeStyle: 'short'});
+}
+
+let fullTimeShow = setInterval(function(){timerFullFormat()},250);
+
+let shortTimeShow = setInterval(function(){timerShortFormat()},1000);
+
+togBtn.addEventListener('click', function(event){
+    fullFormat.classList.toggle('demo-hidden');
+    shortFormat.classList.toggle('demo-visible');
+})
