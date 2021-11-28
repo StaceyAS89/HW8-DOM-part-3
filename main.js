40bd682b364d324656fa593655521bb49a121881
StaceyAS89 <anastasiiaforapple@gmail.com>
2021-11-28 14:00:09 +0200

    HW8 version1.4 with review

--- main.js
+++ main.js
@@ -1,33 +1,21 @@
-let togBtn = document.getElementById('togBtn');
 let fullFormat = document.getElementById("demo-full-format");
-let shortFormat = document.getElementById("demo-short-format");
-
+let timeClick = true;
 
 
 function timerFullFormat() {
     let timeShow = new Date();
-    fullFormat.innerHTML = timeShow.toLocaleTimeString();
-}
-
-function timerShortFormat() {
-    let timeShow = new Date();
-    shortFormat.innerHTML = timeShow.toLocaleTimeString([], {
+    return timeClick ? fullFormat.innerHTML=timeShow.toLocaleTimeString():fullFormat.innerHTML=timeShow.toLocaleTimeString([], {
         timeStyle: 'short'
-    });
+    })
 }
 
-let fullTimeShow = setInterval(function () {
-    timerFullFormat()
-}, 250);
+fullFormat.addEventListener('click', function(event){
+    timeClick=!timeClick
 
-let shortTimeShow = setInterval(function () {
-    timerShortFormat()
-}, 1000);
-
-togBtn.addEventListener('click', function (event) {
-    fullFormat.classList.toggle('demo-hidden');
-    shortFormat.classList.toggle('demo-visible');
 })
+setInterval(function() {
+    timerFullFormat();
+}, 250)
 
 function getNumbers(from, to) {
     if (from > to) {
