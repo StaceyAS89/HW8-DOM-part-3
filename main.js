fa4756c1530034163c9a5608384480dc2316e335
StaceyAS89 <anastasiiaforapple@gmail.com>
2021-11-28 14:02:23 +0200

    HW8 version1.4 with review

--- main.js
+++ main.js
@@ -4,27 +4,28 @@ let timeClick = true;
 
 function timerFullFormat() {
     let timeShow = new Date();
-    return timeClick ? fullFormat.innerHTML=timeShow.toLocaleTimeString():fullFormat.innerHTML=timeShow.toLocaleTimeString([], {
+    return timeClick ? fullFormat.innerHTML = timeShow.toLocaleTimeString() : fullFormat.innerHTML = timeShow.toLocaleTimeString([], {
         timeStyle: 'short'
     })
 }
 
-fullFormat.addEventListener('click', function(event){
-    timeClick=!timeClick
-
-})
-setInterval(function() {
+setInterval(function () {
     timerFullFormat();
 }, 250)
 
+fullFormat.addEventListener('click', function (event) {
+    timeClick = !timeClick
+
+})
+
 function getNumbers(from, to) {
     if (from > to) {
-      return
-    } 
+        return
+    }
     console.log(from)
     from += 1;
-    
-    getNumbers(from, to)
-  }
 
-  getNumbers(5, 20)
+    getNumbers(from, to)
+}
+
+getNumbers(5, 20)
