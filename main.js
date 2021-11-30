944a64651aa628b2eddde66fce27780daf1e18a6
StaceyAS89 <anastasiiaforapple@gmail.com>
2021-11-30 18:45:00 +0200

    HW9 version 3

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let timeClick = true;
 
 function timerFullFormat() {
     let timeShow = new Date();
-    return timeClick ? fullFormat.innerHTML = timeShow.toLocaleTimeString() : fullFormat.innerHTML = timeShow.toLocaleTimeString([], {
+    fullFormat.innerHTML = timeClick ? timeShow.toLocaleTimeString() : timeShow.toLocaleTimeString([], {
         timeStyle: 'short'
     })
 }
@@ -17,15 +17,3 @@ fullFormat.addEventListener('click', function (event) {
     timeClick = !timeClick
 
 })
-
-function getNumbers(from, to) {
-    if (from > to) {
-        return
-    }
-    console.log(from)
-    from += 1;
-
-    getNumbers(from, to)
-}
-
-getNumbers(5, 20)
